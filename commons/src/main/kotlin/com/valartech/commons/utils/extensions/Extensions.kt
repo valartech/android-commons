e15ad20e4c6257864d9931beced536bcb6973833
Amitav Khandelwal <amitav.khandelwal@gmail.com>
2019-05-01 23:02:22 +0530

    Updated auth header, added extensions

--- commons/src/main/kotlin/com/valartech/commons/utils/extensions/Extensions.kt
+++ commons/src/main/kotlin/com/valartech/commons/utils/extensions/Extensions.kt
@@ -2,9 +2,13 @@ package com.valartech.commons.utils.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.valartech.commons.R
 import java.text.DecimalFormat
@@ -47,4 +51,42 @@ fun View.setVisibility(isVisible: Boolean, setInvisible: Boolean = false) {
             View.GONE
         }
     }
+}
+
+fun EditText.showKeyboard(context: Context) {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+}
+
+/**
+ * https://stackoverflow.com/a/10947374/3460025
+ */
+fun TextView.underline() {
+    this.paintFlags = paintFlags or Paint.UNDERLINE_TEXT_FLAG
+}
+
+/**
+ * https://stackoverflow.com/a/6796829/3460025
+ */
+fun TextView.removeUnderline() {
+    this.paintFlags = paintFlags and Paint.UNDERLINE_TEXT_FLAG.inv()
+}
+
+/**
+ * https://stackoverflow.com/a/18669307/3460025
+ */
+fun View.getAllChildren(): List<View> {
+    val visited = ArrayList<View>()
+    val unvisited = ArrayList<View>()
+    unvisited.add(this)
+
+    while (unvisited.isNotEmpty()) {
+        val child = unvisited.removeAt(0)
+        visited.add(child)
+        if (child !is ViewGroup) continue
+        val childCount = child.childCount
+        for (i in 0 until childCount) unvisited.add(child.getChildAt(i))
+    }
+
+    return visited
 }
