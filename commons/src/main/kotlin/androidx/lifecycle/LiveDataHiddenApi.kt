a9a508c1324a1defa643e445c60b86f0cf862550
Harshil Shah <shahharshil0@yahoo.com>
2019-06-27 19:58:21 +0530

    added classCastException to unCheckedCast warning

--- commons/src/main/kotlin/androidx/lifecycle/LiveDataHiddenApi.kt
+++ commons/src/main/kotlin/androidx/lifecycle/LiveDataHiddenApi.kt
@@ -3,6 +3,7 @@ package androidx.lifecycle
 /**
  * https://github.com/kxfeng/livedata-ext/blob/master/library/src/main/java/androidx/lifecycle/LiveDataHiddenApi.kt
  */
+@Suppress("UndocumentedPublicFunction")
 object LiveDataHiddenApi {
     fun LiveData<*>.version(): Int {
         return this.version
