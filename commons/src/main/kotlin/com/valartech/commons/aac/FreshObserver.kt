e8a3989bcedd4f5c98271a3b07cf4e9ae26eacae
Harshil Shah <shahharshil0@yahoo.com>
2019-06-27 20:09:40 +0530

    suppressed UnsafeCast

--- commons/src/main/kotlin/com/valartech/commons/aac/FreshObserver.kt
+++ commons/src/main/kotlin/com/valartech/commons/aac/FreshObserver.kt
@@ -7,6 +7,8 @@ import timber.log.Timber
  * A wrapped observer which will only send changed data to delegate when predicate is true.
  * If two [delegate] are equal, their wrapped FreshObserver are ensured to be equal.
  */
+//have added this because even after adding try catch codacy gives warning
+@Suppress("UnsafeCast")
 internal class FreshObserver<T>(
     private val delegate: Observer<in T>,
     private val predicate: FreshPredicate
