342a33234b8f1fb808356c09911151ba5b0311ba
iamkdblue <iamkdblue@gmail.com>
2021-09-17 19:26:43 +0530

    removed loading layout

--- app/src/main/java/com/valartech/common/MainActivity.kt
+++ app/src/main/java/com/valartech/common/MainActivity.kt
@@ -1,29 +1,12 @@
 package com.valartech.common
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.valartech.commons.views.LoadingLayout.Companion.COMPLETE
-import com.valartech.commons.views.LoadingLayout.Companion.EMPTY
-import com.valartech.commons.views.LoadingLayout.Companion.ERROR
-import com.valartech.commons.views.LoadingLayout.Companion.LOADING
-import com.valartech.commons.views.LoadingLayout.Companion.LOADING_OVERLAY
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        loading.setOnClickListener { mainLoadingLayout.setState(LOADING) }
-        loadingOverlay.setOnClickListener { mainLoadingLayout.setState(LOADING_OVERLAY) }
-        complete.setOnClickListener { mainLoadingLayout.setState(COMPLETE) }
-        empty.setOnClickListener { mainLoadingLayout.setState(EMPTY) }
-        error.setOnClickListener { mainLoadingLayout.setState(ERROR) }
-
-        test_button.setOnClickListener {
-            Toast.makeText(this, "Test button pressed!", Toast.LENGTH_SHORT).show()
-        }
     }
 }
